2264977a593969af81c2b595502c00f88b395af0
Andrew Waite <andrew.waite@infosanity.co.uk>
2020-12-12 09:23:06 +0000

    Project refresh

---
+++ access-key-pot.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_user" "honeyuser" {
+  name = "DeeSeatful"
+  tags = {
+    Division = "Obfuscation"
+  }
+}
+
+resource "aws_iam_user_policy" "honeyuser_policy" {
+  name = "explicit_deny"
+  user = "${aws_iam_user.honeyuser.name}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "*",
+            "Effect": "Deny",
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_access_key" "honeyuser_key" {
+  user = "${aws_iam_user.honeyuser.name}"
+}
+
+output "secret" {
+  value = "${aws_iam_access_key.honeyuser_key.secret}"
+}
+
+output "key" {
+  value = "${aws_iam_access_key.honeyuser_key.id}"
+}
